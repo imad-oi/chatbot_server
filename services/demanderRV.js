9036c2990f67d2f9e83a2d0101b332f96ca37ce4
imad-oi <oissafe.imad@gmail.com>
2023-03-07 23:45:40 +0100

    ...

---
+++ services/demanderRV.js
@@ -0,0 +1,9 @@
+// user  : je veux demander un RV , rv , intent : demander_rv
+// bot  : avec plisair, voici les sujets disponible [ static ] , que vous prefere ? 
+ 
+
+// intent2 : recuper_sujet_rv 
+//  7eme module 
+// bot :   votre sujet : $sujet_rv est enregistrer avec sucess 
+
+
