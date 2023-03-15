d75fe5e991a9a80ce3a808dfa3da67070256370c
imad-oi <oissafe.imad@gmail.com>
2023-03-15 18:06:43 +0100

    ..

--- server.js
+++ server.js
@@ -13,38 +13,10 @@ app.use(bodyParser.json());
 app.use(router)
 
 app.get("/" , (req , res)=>{
-    // res.status(200).send({
-    //     message:"hello from server"
-    // }) ;
-    res.sendFile(path.join(__dirname+'/pdf.html'))
+    res.status(200).send({
+        message:"hello from server"
+    }) ;
 })
-
-// app.get('/download-pdf', (req, res) => {
-//     // create a new PDF document
-//     const doc = new PDFDocument();
-//     // set the PDF document's title
-//     doc.info.Title = 'Releve de notes  ';
-//     // write some text to the PDF document
-//     doc.text('Releve de notes');
-    
-//     const table = {
-//       headers: ['Name', 'Grade'],
-//       rows: [
-//         ['John', 'A'],
-//         ['Mary', 'B'],
-//         ['Tom', 'C']
-//       ]
-//     };
-//     // add the table to the PDF document
-//     // doc.table(table);
-//     // set the HTTP headers to indicate that a PDF file should be downloaded
-//     res.setHeader('Content-Type', 'application/pdf');
-//     res.setHeader('Content-Disposition', 'attachment; filename=releve_note.pdf');
-//     // pipe the PDF document to the response stream
-//     doc.pipe(res);
-//     // finalize the PDF document
-//     doc.end();
-//   });
 
 
 
